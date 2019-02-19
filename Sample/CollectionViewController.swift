22d9ad4a5e63c10d4415cf8e67609ec0c973c556
Rinni Swift <rinradaswift@gmail.com>
2019-02-19 15:00:46 -0800

    sample json data file

---
+++ Sample/CollectionViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CollectionViewController.swift
+//  Squirrel
+//
+//  Created by Rinni Swift on 2/19/19.
+//  Copyright © 2019 Rinni Swift. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Sample/SampleCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  SampleCollectionViewCell.swift
+//  Squirrel
+//
+//  Created by Rinni Swift on 2/19/19.
+//  Copyright © 2019 Rinni Swift. All rights reserved.
+//
+
+import UIKit
+
+class SampleCollectionViewCell: UICollectionViewCell {
+    
+}
