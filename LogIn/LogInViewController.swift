d65f3f805fd66b8c47915883dce1b4e509a0b2ee
Rinni Swift <rinradaswift@gmail.com>
2019-02-21 13:37:16 -0800

    new implementation of the login view controller UI

--- LogIn/LogInViewController.swift
+++ LogIn/LogInViewController.swift
@@ -10,21 +10,10 @@ import UIKit
 
 class LogInViewController: UIViewController {
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        view.setGradientView(colorOne: .smoothBlueBG, colorTwo: .smoothWhiteBG)
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
