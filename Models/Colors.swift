80b3315ebb07c9256d38439dc3535c6959790272
Rinni Swift <rinradaswift@gmail.com>
2019-02-21 13:34:52 -0800

    added more uicolors

--- Models/Colors.swift
+++ Models/Colors.swift
@@ -12,4 +12,7 @@ import UIKit
 extension UIColor {
     static let mainPurple = UIColor(red: 180/255, green: 185/255, blue: 208/255, alpha: 1)
     static let smoothWhite = UIColor(red: 253/255, green: 253/255, blue: 253/255, alpha: 1)
+    
+    static let smoothWhiteBG = UIColor(red: 245/255, green: 245/255, blue: 245/255, alpha: 1)
+    static let smoothBlueBG = UIColor(red: 235/255, green: 243/255, blue: 255/255, alpha: 1)
 }
---
+++ Models/Gradient.swift
@@ -0,0 +1,9 @@
+//
+//  Gradient.swift
+//  Squirrel
+//
+//  Created by Rinni Swift on 2/21/19.
+//  Copyright © 2019 Rinni Swift. All rights reserved.
+//
+
+import Foundation
