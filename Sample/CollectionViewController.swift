20da6b0e579cb15e1a6293e99df7065d6f8fdca8
Rinni Swift <rinradaswift@gmail.com>
2019-02-20 22:04:05 -0800

    added collection view header for each section which will contain 4 cells

--- Sample/CollectionViewController.swift
+++ Sample/CollectionViewController.swift
@@ -10,7 +10,11 @@ import UIKit
 
 class CollectionViewController: UIViewController, UICollectionViewDelegateFlowLayout {
     
+    // MARK: - Outlets
     @IBOutlet weak var collectionView: UICollectionView!
+    
+    // MARK: - Variables
+    var sampleSectionTitle: [String] = ["SPD 1.2", "Healthy Recipes", "Sports"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,29 +27,36 @@ class CollectionViewController: UIViewController, UICollectionViewDelegateFlowLa
     
     func flowLayout() {
         let layout =  collectionView.collectionViewLayout as! UICollectionViewFlowLayout
-        let width = view.frame.size.width / 3
-        layout.itemSize = CGSize(width: width, height: width)
+        let width = (view.frame.size.width - 40) / 2
+        let height = (view.frame.size.width - 20) / 3
+        layout.itemSize = CGSize(width: width, height: height)
     }
 
 }
 
 extension CollectionViewController: UICollectionViewDelegate, UICollectionViewDataSource {
     
-    func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return 3
-    }
-    
-//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
-//        <#code#>
-//    }
-    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 15
+        return 4
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "something", for: indexPath) as! SampleCollectionViewCell
-        cell.backgroundColor = .red
+        cell.backgroundColor = .mainPurple
+        cell.layer.cornerRadius = 10
         return cell
+    }
+    
+    
+    // SECTION HEADER
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return sampleSectionTitle.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        let header = collectionView.dequeueReusableSupplementaryView(ofKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: "something", for: indexPath) as! SampleCollectionReusableView
+        header.categoryTitle.text = sampleSectionTitle[indexPath.section]
+        return header
     }
 }
---
+++ Sample/SampleCollectionReusableView.swift
@@ -0,0 +1,14 @@
+//
+//  SampleCollectionReusableView.swift
+//  Squirrel
+//
+//  Created by Rinni Swift on 2/20/19.
+//  Copyright © 2019 Rinni Swift. All rights reserved.
+//
+
+import UIKit
+
+class SampleCollectionReusableView: UICollectionReusableView {
+    
+    @IBOutlet weak var categoryTitle: UILabel!
+}
