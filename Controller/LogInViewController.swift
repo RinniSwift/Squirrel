7edcf482ac817838b09cf0b3f6a8c64eed015a44
Rinni Swift <rinradaswift@gmail.com>
2019-02-26 16:03:03 -0800

    added keyboard notification listeners

--- Controller/LogInViewController.swift
+++ Controller/LogInViewController.swift
@@ -10,15 +10,50 @@ import UIKit
 
 class LogInViewController: UIViewController {
 
+    // MARK: - Outlets
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.setGradientView(colorOne: .smoothBlueBG, colorTwo: .smoothWhiteBG)
+        keyboardListenEvents()
         
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         self.view.endEditing(true)
     }
+    
+    // MARK: - Functions
+    func keyboardListenEvents() {
+        // listen for keyboard notifications
+        emailTextField.delegate = self
+        passwordTextField.delegate = self
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillChange(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillChange(notification:)), name: UIResponder.keyboardDidHideNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillChange(notification:)), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
+    }
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
+    }
+    
+    @objc func keyboardWillChange(notification: Notification) {
+        guard let keyboardRect = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue else { return }
+        
+        if notification.name == UIResponder.keyboardWillShowNotification || notification.name == UIResponder.keyboardWillHideNotification {
+            view.frame.origin.y = -(keyboardRect.height - 50)
+        } else {
+            view.frame.origin.y = 0
+        }
+    }
+}
 
+
+extension LogInViewController: UITextFieldDelegate {
+    
 }
