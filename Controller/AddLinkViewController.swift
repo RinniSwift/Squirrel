603e89f7ed5a8b73049e9b338a381c545c62784e
Rinni Swift <rinradaswift@gmail.com>
2019-03-05 20:30:25 -0800

    added imagePickerController for adding images to a new card within the category

--- Controller/AddLinkViewController.swift
+++ Controller/AddLinkViewController.swift
@@ -7,11 +7,13 @@
 //
 
 import UIKit
+import Photos
 
-class AddLinkViewController: UIViewController {
+class AddLinkViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     // MARK: - Variables
     var imageArray = [UIImage]()
+    var imagePicker = UIImagePickerController()
 
     // MARK: - Actions
     @IBAction func saveButtonTapped(_ sender: UIButton) {
@@ -20,6 +22,12 @@ class AddLinkViewController: UIViewController {
     @IBAction func cancelButtonTapped(_ sender: UIButton) {
         self.dismiss(animated: true, completion: nil)
     }
+    
+    
+    @IBAction func addImageButtonTapped(_ sender: UIButton) {
+        openPhotoLibrary()
+    }
+    
     
     // MARK: - Outlets
     @IBOutlet weak var collectionView: UICollectionView!
@@ -33,6 +41,7 @@ class AddLinkViewController: UIViewController {
         collectionView.delegate = self
         collectionView.dataSource = self
         flowLayout()
+        imagePicker.delegate = self
     }
     
     func flowLayout() {
@@ -41,17 +50,45 @@ class AddLinkViewController: UIViewController {
         let height = (collectionView.frame.size.width / 2) + 30
         layout.itemSize = CGSize(width: width, height: height)
     }
+    
+    func openPhotoLibrary() {
+        PHPhotoLibrary.requestAuthorization { status in
+            switch status {
+            case .authorized:
+                DispatchQueue.main.async {
+                    self.imagePicker.sourceType = .photoLibrary
+                    self.imagePicker.allowsEditing = true
+                    self.present(self.imagePicker, animated: true, completion: nil)
+                }
+            case .notDetermined:
+                print("not determined")
+            case .restricted:
+                print("restricted")
+            case .denied:
+                print("denied")
+            }
+        }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let imgPicked = info[UIImagePickerController.InfoKey.editedImage] as? UIImage {
+            dismiss(animated: true, completion: nil)
+            imageArray.append(imgPicked)
+            collectionView.reloadData()
+        }
+    }
 }
 
 extension AddLinkViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 3
+        return imageArray.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellImage", for: indexPath) as! AddImageCollectionViewCell
+        cell.cellImage.image = imageArray[indexPath.item]
         return cell
     }
-    
     
 }
