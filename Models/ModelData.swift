8fe8de9fa16f31a3dea55b03681b7f772454b1fe
Rinni Swift <rinradaswift@gmail.com>
2019-03-07 22:00:59 -0800

    added model for category names

---
+++ Models/ModelData.swift
@@ -0,0 +1,15 @@
+//
+//  Models.swift
+//  Squirrel
+//
+//  Created by Rinni Swift on 3/7/19.
+//  Copyright © 2019 Rinni Swift. All rights reserved.
+//
+
+import Foundation
+
+class ModelData {
+    
+    var modelCategoryNames: [String]?
+    
+}
